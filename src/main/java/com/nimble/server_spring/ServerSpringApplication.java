b86463ff06c54b98d85abcb6eac8dd34982d9934
JadenKim-dev <jaden.kim5179@gmail.com>
2023-08-28 20:21:43 +0900

    style: 코드 포맷팅 적용

--- src/main/java/com/nimble/server_spring/ServerSpringApplication.java
+++ src/main/java/com/nimble/server_spring/ServerSpringApplication.java
@@ -8,8 +8,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class ServerSpringApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ServerSpringApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(ServerSpringApplication.class, args);
+  }
 
 }
