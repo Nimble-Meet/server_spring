318ef327ecd6b3e43e8c319b25227f6d7315268b
JadenKim-dev <jaden.kim5179@gmail.com>
2023-11-12 21:07:03 +0900

    feat(socket): web socket 연결 관련 인증 로직 추가
    - jwt 토큰 기반으로 소켓 connect 시 인증 처리를 수행하도록 Interceptor 로직 구현
    - socket subscribe 및 publish 시 인증을 수행하도록 SecurityConfig 설정 추가

---
+++ src/main/java/com/nimble/server_spring/infra/security/WebSocketSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.nimble.server_spring.infra.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
+import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
+
+@Configuration
+public class WebSocketSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
+
+    @Override
+    protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
+        messages.nullDestMatcher().permitAll()
+            .simpSubscribeDestMatchers("/subscribe/**").authenticated()
+            .simpDestMatchers("/publish/**").authenticated()
+            .anyMessage().denyAll();
+    }
+
+    @Override
+    protected boolean sameOriginDisabled() {
+        return true;
+    }
+}
--- src/main/java/com/nimble/server_spring/infra/socket/WebSocketConfig.java
+++ src/main/java/com/nimble/server_spring/infra/socket/WebSocketConfig.java
@@ -1,14 +1,22 @@
 package com.nimble.server_spring.infra.socket;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+@Order(Ordered.HIGHEST_PRECEDENCE + 99)
 @Configuration
 @EnableWebSocketMessageBroker
-public class SocketConfig implements WebSocketMessageBrokerConfigurer {
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final WebSocketInterceptor webSocketInterceptor;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -21,4 +29,8 @@ public class SocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.setApplicationDestinationPrefixes("/publish");
     }
 
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(webSocketInterceptor);
+    }
 }
