be1d1916d22387a49129a1d40a5fccc2839064bb
JadenKim-dev <jaden.kim5179@gmail.com>
2024-01-23 22:06:39 +0900

    - 로그인 로직의 쿼리 최적화     - LocalLoginAuthenticationManager에서 userId를 principal로 전달하도록 변경     - LocalLoginSuccessHandler에서는 authentication에 담긴 정보를 최대한 사용하여, 추가적인 User 조회 없이 토큰을 발급하도록 수정     - JwtToken과 User 사이에 Lazy Loading 적용

--- src/main/java/com/nimble/server_spring/infra/security/LocalLoginAuthenticationManager.java
+++ src/main/java/com/nimble/server_spring/infra/security/LocalLoginAuthenticationManager.java
@@ -7,15 +7,13 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @RequiredArgsConstructor
 @Slf4j
 public class LocalLoginAuthenticationManager implements AuthenticationManager {
 
-    private final UserDetailsService userDetailsService;
+    private final CustomUserDetailsService userDetailsService;
     private final PasswordEncoder passwordEncoder;
 
     @Override
@@ -25,13 +23,14 @@ public class LocalLoginAuthenticationManager implements AuthenticationManager {
         String email = String.valueOf(authentication.getPrincipal());
         String password = String.valueOf(authentication.getCredentials());
 
-        UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+        CustomUserDetails userDetails =
+            (CustomUserDetails) userDetailsService.loadUserByUsername(email);
         if (!passwordEncoder.matches(password, userDetails.getPassword())) {
             throw new BadCredentialsException("비밀번호가 일치하지 않습니다.");
         }
 
         return new UsernamePasswordAuthenticationToken(
-            userDetails.getUsername(),
+            userDetails.getUserId(),
             userDetails.getPassword(),
             userDetails.getAuthorities()
         );
