8a5d2d586f22bba58ad7386d12b8d17ea6c50bf4
JadenKim-dev <jaden.kim5179@gmail.com>
2024-03-14 21:13:56 +0900

    test(Auth): publishJwtToken에 대한 테스트 코드 작성
    - 신규 발급자, 기발급자 테스트 케이스를 구분

--- src/test/java/com/nimble/server_spring/modules/auth/AuthServiceTest.java
+++ src/test/java/com/nimble/server_spring/modules/auth/AuthServiceTest.java
@@ -3,16 +3,19 @@ package com.nimble.server_spring.modules.auth;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.tuple;
-import static org.junit.jupiter.api.Assertions.*;
 
 import com.nimble.server_spring.IntegrationTestSupport;
 import com.nimble.server_spring.infra.error.ErrorCode;
 import com.nimble.server_spring.infra.error.ErrorCodeException;
+import com.nimble.server_spring.infra.security.RoleType;
 import com.nimble.server_spring.modules.auth.dto.request.LocalSignupServiceRequest;
+import com.nimble.server_spring.modules.auth.dto.request.PublishTokenServiceRequest;
+import com.nimble.server_spring.modules.auth.dto.response.JwtTokenResponse;
 import com.nimble.server_spring.modules.auth.dto.response.UserResponse;
 import com.nimble.server_spring.modules.auth.enums.OauthProvider;
 import com.nimble.server_spring.modules.user.User;
 import com.nimble.server_spring.modules.user.UserRepository;
+import java.time.LocalDateTime;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -65,11 +68,7 @@ class AuthServiceTest extends IntegrationTestSupport {
     @Test
     void signupWithExistingEmail() {
         // given
-        User user = User.createLocalUser(
-            "user@email.com",
-            "$2a$12$SE54wBhXbyHzeicLdzFK5OOvuJq0mg29ScXLIeDEjeQoGLlJylDb6",
-            "user1"
-        );
+        User user = createLocalUser("user@email.com");
         userRepository.save(user);
 
         LocalSignupServiceRequest localSignupRequest = LocalSignupServiceRequest.builder()
@@ -83,5 +82,84 @@ class AuthServiceTest extends IntegrationTestSupport {
         assertThatThrownBy(() -> authService.signup(localSignupRequest))
             .isInstanceOf(ErrorCodeException.class)
             .hasMessage(ErrorCode.EMAIL_ALREADY_EXISTS.getMessage());
+    }
+
+    @DisplayName("기존에 토큰을 발급한 적이 없는 유저이면 토큰을 발급한 후 저장한다.")
+    @Test
+    void publishJwtTokenNotExisting() {
+        // given
+        User user = createLocalUser("user@email.com");
+        userRepository.save(user);
+
+        PublishTokenServiceRequest publishTokenRequest = PublishTokenServiceRequest.create(
+            user.getId(),
+            RoleType.USER
+        );
+
+        // when
+        JwtTokenResponse jwtTokenResponse = authService.publishJwtToken(publishTokenRequest);
+
+        // then
+        assertThat(jwtTokenResponse).isNotNull()
+            .extracting("accessToken", "refreshToken")
+            .allSatisfy(token -> assertThat(token).isNotNull());
+        assertThat(jwtTokenResponse.getUser().getEmail()).isEqualTo("user@email.com");
+
+        List<JwtToken> jwtTokens = jwtTokenRepository.findAll();
+        assertThat(jwtTokens).hasSize(1)
+            .extracting("accessToken", "refreshToken")
+            .containsExactlyInAnyOrder(
+                tuple(jwtTokenResponse.getAccessToken(), jwtTokenResponse.getRefreshToken())
+            );
+    }
+
+    @DisplayName("기존에 토큰을 발급한 적이 있는 유저이면 토큰을 발급한 후 기존의 데이터를 수정한다.")
+    @Test
+    void publishJwtTokenExisting() {
+        // given
+        User user = createLocalUser("user@email.com");
+        userRepository.save(user);
+
+        JwtToken jwtToken = createJwtToken(user);
+        jwtTokenRepository.save(jwtToken);
+
+        PublishTokenServiceRequest publishTokenRequest = PublishTokenServiceRequest.create(
+            user.getId(),
+            RoleType.USER
+        );
+
+        // when
+        JwtTokenResponse jwtTokenResponse = authService.publishJwtToken(publishTokenRequest);
+
+        // then
+        assertThat(jwtTokenResponse).isNotNull()
+            .extracting("accessToken", "refreshToken")
+            .allSatisfy(token -> assertThat(token).isNotNull());
+        assertThat(jwtTokenResponse.getUser().getEmail()).isEqualTo("user@email.com");
+
+        List<JwtToken> jwtTokens = jwtTokenRepository.findAll();
+        assertThat(jwtTokens).hasSize(1)
+            .extracting("accessToken", "refreshToken")
+            .containsExactlyInAnyOrder(
+                tuple(jwtTokenResponse.getAccessToken(), jwtTokenResponse.getRefreshToken())
+            );
+    }
+
+
+    private static User createLocalUser(String email) {
+        return User.createLocalUser(
+            email,
+            "$2a$12$SE54wBhXbyHzeicLdzFK5OOvuJq0mg29ScXLIeDEjeQoGLlJylDb6",
+            "user1"
+        );
+    }
+
+    private static JwtToken createJwtToken(User user) {
+        return JwtToken.builder()
+            .accessToken("accessToken")
+            .refreshToken("refreshToken")
+            .expiresAt(LocalDateTime.now().plusDays(1))
+            .user(user)
+            .build();
     }
 }
